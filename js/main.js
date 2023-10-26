6b1b372d58831fa0937b9ca2e5b442802af2fba4
vickyaltitude <vignvick3005@gmail.com>
2023-10-26 22:07:19 +0530

    new update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log(typeof("Hi I'm vignesh"))
+console.log("Hi im vignesh")
+const myVar = "education";
+console.log(myVar)
+console.log(myVar.length)
+console.log(myVar.slice(2,3))
+console.log(myVar.indexOf("at"))
+console.log(myVar.split("t"))
+const myInt = 50;
+console.log(Number.parseFloat(myInt));
