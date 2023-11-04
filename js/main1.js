46e3fab8daf671b8767e892ae4daebbacb2fb69c
vickyaltitude <vignvick3005@gmail.com>
2023-11-04 12:30:31 +0530

    new file

---
+++ js/main1.js
@@ -0,0 +1,16 @@
+function addition(){
+    let firstChoice = 50;
+    let secondChoice = 50;
+    result = firstChoice + secondChoice;
+    console.log(result);
+}
+addition();
+
+function subtract(){
+    let firstNum = 50;
+    let secondNum = 50;
+    result2 = firstNum - secondNum;
+    console.log(result2);
+}
+subtract();
+
