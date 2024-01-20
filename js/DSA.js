b838e0f30f679f0d3b554977fe8291b200465ad3
vickyaltitude <vignvick3005@gmail.com>
2024-01-20 22:31:52 +0530

    frequency counter pattern and multiple pointer pattern

--- js/DSA.js
+++ js/DSA.js
@@ -1,25 +1,15 @@
-//KMP(Knut-Morris-Pratt) Algorithm
+//KMP(Knut-Morris-Pratt) Algorithm - is used to get a substring index from the given string with time complexity O(n+m)
 
-function returnSubInd(str,tar){
-   
-    for(let i=0;i<str.length;i++){
-        for(let j=0;j<tar.length;j++){
-            if(str[i] == tar[j]){
+function kmp(str,sub){
 
-               if(str[i] != tar[j] || j == tar.length-1){
-                return [i-j,i]
-               }
-               i++
-               continue
-            }
-            
-        }
-    }
-    
-    return "No matching str"
+
+
 }
 
-console.log(returnSubInd("javascript","script"))
+console.log(kmp("ababacadabab","acad"))
+
+
+
 
 
 let myOb = {vignesh: 22,place: "india"}
@@ -110,7 +100,7 @@ let newStr = "Hello Hi there how are u? $23"
 console.log(newStr.replace(/[^a-zA-Z0-9]/g,""));
 
 
-
+//selection sort
 //Given an unsorted array of integers, write a function that returns the minimum number of swaps required to sort the array using the selection sort algorithm.
 //Example
 //Input: [7, 5, 2, 6, 3]
@@ -143,16 +133,150 @@ console.log(selectionReq([7, 5, 2, 6, 3]));
 
 
 
+//frequency counter method in algorithm
+//since we used indexof method in below code it is considered as naive solution and hence it is not the best possible solution
+
+/* function same(arr1,arr2){
+
+    if(arr1.length != arr2.length){
+        return false
+    }
+    let i=0;
+    while(i<arr1.length){
+        if(arr2.indexOf(arr1[i]*arr1[i]) != -1){
+            arr2.splice(arr2.indexOf(arr1[i]*arr1[i]),1);
+            if(!arr2.length){
+                return true
+            }
+        }
+        else if(arr2.indexOf(arr1[i]*arr1[i]) == -1){
+            return false
+        }
+        i++
+    }
+}
+console.log(same([1,2,3], [4,1,9])) // true
+console.log(same([1,2,3], [1,9])) // false
+console.log(same([1,2,1], [4,4,1])) // false (must be same frequency) */
+
+//here we use 3 seperated loops instead of using indexof method inside a for loop which leads to nested loop and quadratic situation
+//hence the below is the best possible solution for the problem and hence its called frequency counter.
+/* function same(arr1,arr2){
+    let freq1 = {};
+    let freq2 = {};
+
+    for(let val of arr1){
+        freq1[val] = (freq1[val] || 0) + 1;
+    }
+    for(let val of arr2){
+        freq2[val] = (freq2[val] || 0) + 1;
+    }
+    for(let key in freq1){
+        if(!(key*key) in freq2){
+            return false
+        }
+        if(freq2[key*key] != freq1[key]){
+            return false
+        }
+    }
+    return true;
+
+}
+console.log(same([1,2,2,3], [4,1,9,4])) // true
+console.log(same([1,2,3], [1,9])) // false
+console.log(same([1,2,1], [4,4,1])) // false (must be same frequency) */
 
 
+//now we see another problems that has the opportunity to solve with the help of frequency counter.
+
+//check the given words are anagram
+
+/* function validAnagram(str1,str2){
+    let firstStr = {};
+    let secondStr = {};
+    for(let val of str1){
+        firstStr[val] = (firstStr[val] ||0) +1    
+    }
+    for(let val of str2){
+        secondStr[val] = (secondStr[val] ||0) +1    
+    }
+
+    for(let key in firstStr){
+        if(!key in secondStr){
+            return false
+        }
+        if(firstStr[key] != secondStr[key]){
+            return false
+        }
+    }
+    return true;
+}
+
+console.log(validAnagram ('', ''))// true
+console.log(validAnagram ('aaz', 'zza')) // false
+console.log(validAnagram('anagram', 'nagaram')) // true
+console.log(validAnagram( "rat", "car")) // false)
+console.log(validAnagram( 'awesome', 'awesom')) // false
+console.log(validAnagram ('qwerty', 'qeywrt')) // true
+console.log(validAnagram('texttwisttime', 'timetwisttext')) // true */
+
+//this is another best solution using two loops only.
+
+function validAnagram (first, second) {
+    if (first.length !== second. length) {
+    return false;
+    }
+    const lookup = {};
+    
+    for (let i = 0;i< first. length; i++) {
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup [letter] ? lookup [letter] += 1 : lookup [letter] = 1;
+    }
+    for (let i = 0; i< second. length; i++) {
+    let letter = second [i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup [letter]) {
+    return false;
+    } else {
+    lookup [letter] -= 1;
+    }
+    }
+    return true;
+    }
+
+console.log(validAnagram ('', ''))// true
+console.log(validAnagram ('aaz', 'zza')) // false
+console.log(validAnagram('anagram', 'nagaram')) // true
+console.log(validAnagram( "rat", "car")) // false)
+console.log(validAnagram( 'awesome', 'awesom')) // false
+console.log(validAnagram ('qwerty', 'qeywrt')) // true
+console.log(validAnagram('texttwisttime', 'timetwisttext')) // true
 
 
+//multiple pointer
+//example - write a function called sumZero which accepts sorted array of integers. the function should find the firstpair where the sum is 0. return an array that includes both values that sum to zero or undefined if a pair does not exist.
 
+function sumZero(arr,tar){
+    let firstPointer = 0;
+    let lastPointer = arr.length-1;
 
+    while(firstPointer<lastPointer){
+        if(arr[firstPointer]+arr[lastPointer]==tar){
+            return [arr[firstPointer],arr[lastPointer]];
+        }else if(arr[firstPointer]+arr[lastPointer]>tar){
+            lastPointer--
+        }else {
+           
+            firstPointer++
+        }
+    }
 
-
-
-
+    return undefined;
+}
+console.log(sumZero ([-4,-3, -2,-1,0, 1,2,3,10],1)) // [-2,3]
+console.log(sumZero ( [-2,0,1,3],5)) // undefined
+console.log(sumZero ([1,2,3],3)) // undefined 
 
 
 
