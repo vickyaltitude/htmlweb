61298ffffc1ec21842e0257a4a67ff138bf9df40
vickyaltitude <vignvick3005@gmail.com>
2024-01-26 22:48:33 +0530

    working on sliding window algorith

--- js/DSA.js
+++ js/DSA.js
@@ -435,4 +435,48 @@ function maxSubarraySum(arr,tar){
   console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
   console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
   console.log(maxSubarraySum([2,3], 3)) // null
+
+
+  //Sliding Window - minSubArrayLen
+/* Write a function called minSubArrayLen which accepts two parameters - an array of positive integers and a positive integer.
+
+This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead. */
+  function minSubArrayLen(arr,n){
+    let firstPointer=0;
+    let secondPointer =0;
+    let minArray = 0;
+    let countCheck =0;
+    let result = 0;
+   
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>=n){
+            return 1
+        }
+    }
+    while(secondPointer<arr.length-1){
+        countCheck = countCheck + arr[secondPointer];
+        minArray++
+       
+        if(countCheck>=n){
+            countCheck = countCheck-arr[firstPointer];
+           firstPointer++
+           result = minArray;
+        }
+        
+        if(secondPointer==arr.length-1){
+            return result
+        }
+        secondPointer++
+    }
+    
+    return 0
+}
+
+console.log(minSubArrayLen([2,3,1,2,4,3], 7)) // 2 -> because [4,3] is the smallest subarray
+console.log(minSubArrayLen([2,1,6,5,4], 9)) // 2 -> because [5,4] is the smallest subarray
+console.log(minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52)) // 1 -> because [62] is greater than 52
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],39)) // 3
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],55)) // 5
+console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)) // 2
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],95)) // 0
   
