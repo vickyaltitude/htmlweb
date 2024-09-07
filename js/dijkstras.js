695353a03580b795426d185ff8da3746b576e455
vickyaltitude <vignvick3005>
2024-09-07 21:37:25 +0530

    learnt dijkstras algorithm

---
+++ js/dijkstras.js
@@ -0,0 +1,45 @@
+class PriorityQueue{
+    constructor(){
+        this.list = [];
+    }
+    enqueue(vertex,priority){
+        this.list.push({vertex,priority});
+        this.sort()
+    };
+    dequeue(){
+        return this.list.shift()
+    }
+    sort(){
+        this.list.sort((a,b)=>{
+            return a.priority - b.priority;
+        })
+    }
+}
+class WeightedGraph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+    addEdges(vertex1,vertex2,weight){
+        this.adjacencyList[vertex1].push({node:vertex2,weight});
+        this.adjacencyList[vertex2].push({node:vertex1,weight})
+    }
+}
+let newGraph = new WeightedGraph();
+newGraph.addVertex("A");
+newGraph.addVertex("B");
+newGraph.addVertex("C");
+newGraph.addVertex("D");
+newGraph.addVertex("E");
+newGraph.addVertex("F");
+newGraph.addEdges("A","B",4)
+newGraph.addEdges("A","C",2)
+newGraph.addEdges("B","E",3)
+newGraph.addEdges("C","D",2)
+newGraph.addEdges("C","F",4)
+newGraph.addEdges("D","E",3)
+newGraph.addEdges("D","F",4)
+newGraph.addEdges("E","F",4)
+console.log(newGraph)
