ca16ea2c697fa74dbf57e128fe3800106e55336a
vickyaltitude <vignvick3005>
2024-09-09 23:47:00 +0530

    implemented dijsktras

--- js/dijkstras.js
+++ js/dijkstras.js
@@ -5,14 +5,12 @@ class PriorityQueue{
     enqueue(vertex,priority){
         this.list.push({vertex,priority});
         this.sort()
-    };
+    }
     dequeue(){
         return this.list.shift()
     }
     sort(){
-        this.list.sort((a,b)=>{
-            return a.priority - b.priority;
-        })
+        this.list.sort((a,b) => a.priority - b.priority);
     }
 }
 class WeightedGraph{
@@ -23,15 +21,16 @@ class WeightedGraph{
         if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
     addEdges(vertex1,vertex2,weight){
-        this.adjacencyList[vertex1].push({node:vertex2,weight});
-        this.adjacencyList[vertex2].push({node:vertex1,weight})
+        this.adjacencyList[vertex1].push({node: vertex2,weight});
+        this.adjacencyList[vertex2].push({node: vertex1,weight})
     }
     dijkstras(start,end){
-        let nodes = new PriorityQueue();
-        let distances = {};
-        let previous = {};
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+        const path = [];
         for(let vertex in this.adjacencyList){
-            if(vertex == start){
+            if(vertex === start){
                 distances[vertex] = 0;
                 nodes.enqueue(vertex,0)
             }else{
@@ -40,9 +39,35 @@ class WeightedGraph{
             }
             previous[vertex] = null;
         }
+        let smallest;
+        while(nodes.list.length){
+            smallest = nodes.dequeue().vertex;
+            if(smallest === end){
+               while(previous[smallest]){
+                 path.push(smallest);
+                 smallest = previous[smallest];
+               }
+               path.push(start)
+               break;
+            }
+            if(smallest || distances[smallest] !== Infinity){
+                for(let neighbour of this.adjacencyList[smallest]){
+                    let candidate = distances[smallest] + neighbour.weight;
+                    let nextNeighbour = neighbour.node;
+                    if(candidate < distances[nextNeighbour]){
+                        distances[nextNeighbour] = candidate;
+                        previous[nextNeighbour] = smallest;
+                        nodes.enqueue(nextNeighbour,candidate);
+                    }
+                }
+
+            }
+
+        }
+        return path.reverse();
     }
 }
-let newGraph = new WeightedGraph();
+const newGraph = new WeightedGraph();
 newGraph.addVertex("A");
 newGraph.addVertex("B");
 newGraph.addVertex("C");
@@ -55,6 +80,6 @@ newGraph.addEdges("B","E",3)
 newGraph.addEdges("C","D",2)
 newGraph.addEdges("C","F",4)
 newGraph.addEdges("D","E",3)
-newGraph.addEdges("D","F",4)
-newGraph.addEdges("E","F",4)
-console.log(newGraph)
+newGraph.addEdges("D","F",1)
+newGraph.addEdges("E","F",1)
+console.log(newGraph.dijkstras("A","E"));
